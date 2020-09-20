fbdfa6922acf34d529af2541a8debef43172cfa5
caowenjing <2226441815@qq.com>
2020-09-20 18:45:21 +0800

    add

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,10 +1,16 @@
+import allure
+import pytest
 class TestLogin:
 
-    def test_login_001(self):
-        assert 0
+    @allure.severity(allure.severity_level.BLOCKER)
+    def test_login1(self):
+        @allure.step(title='输入密码')
+        assert 1
 
-    def test_login_002(self):
-        assert 0
+    def test_login2(self):
+        @allure.step(title='输入用户名')
+        assert 1
 
-    def test_login_003(self):
+    def test_login3(self):
+        @allure.step(title='点击登录')
         assert 1
