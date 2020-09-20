c0e1c5fa8c0e0bd849d79d3510611c9ac63c7c6c
Wendy <2226441815@qq.com>
2020-09-20 18:48:33 +0800

    Update test_login.py

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -2,15 +2,11 @@ import allure
 import pytest
 class TestLogin:
 
-    @allure.severity(allure.severity_level.BLOCKER)
     def test_login1(self):
-        @allure.step(title='输入密码')
         assert 1
 
     def test_login2(self):
-        @allure.step(title='输入用户名')
         assert 1
 
     def test_login3(self):
-        @allure.step(title='点击登录')
         assert 1
